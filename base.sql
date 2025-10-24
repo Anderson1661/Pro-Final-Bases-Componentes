9e14d3b933ed30ac7436af9be116f758dbcfd7a8
Anderson1661 <144566153+Anderson1661@users.noreply.github.com>
2025-10-24 13:06:36 -0500

    base medio

--- base.sql
+++ base.sql
@@ -27,6 +27,73 @@ CREATE DATABASE transportadora;
 
 
 
+CREATE TABLE usuarios ();
+
+CREATE TABLE tipo_usuario (
+	id_tipo_usuario int primary key,
+	descripcion varchar (30)
+);
+
+insert into tipo_usuario values (1,Administrador);
+insert into tipo_usuario values (2,Conductor);
+insert into tipo_usuario values (3,Cliente);
+
+CREATE TABLE cliente (
+	id_cliente int primary key,
+	identificacion varchar(20),
+	id_tipo_identificacion int,
+	nombre varchar(100),
+	direccion varchar(100),
+	id_genero int,
+	codigo_postal varchar(10)
+);
+
+CREATE TABLE administrador (
+
+);
+
+CREATE TABLE conductor (
+
+);
+
+CREATE TABLE tipo_documento (
+	id_tipo_identificacion int primary key,
+	descripcion varchar(30)
+);
+
+CREATE TABLE telefono_cliente (
+	id_cliente int,
+	telefono bigint,
+	primary key (id_cliente, telefono)
+);
+
+CREATE TABLE generos (
+	id_genero int primary key,
+	descripcion varchar(30)
+);
+CREATE TABLE codigo_postal (
+	codigo varchar(10) primary key,
+	ciudad VARCHAR(100) NOT NULL,
+    provincia VARCHAR(100),
+    region VARCHAR(100),
+    id_pais INT NOT NULL
+);
+
+CREATE TABLE pais (
+    id_pais INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE vehiculo ();
+CREATE TABLE tipo_servicio ();
+CREATE TABLE servicios ();
+CREATE TABLE ruta ();
+CREATE TABLE categoria_servicio ();
+CREATE TABLE estado_servicio ();
+CREATE TABLE metodos_pago ();
+
+CREATE TABLE preguntas_seguridad ();
+CREATE TABLE respuestas_seguridad ();
 
 
 
