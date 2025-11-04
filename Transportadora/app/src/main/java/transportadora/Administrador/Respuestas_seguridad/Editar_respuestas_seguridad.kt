725a5532092547ae1d33b326cd6344b80591cdc4
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-04 14:40:37 -0500

    respuestas seguridad full

--- Transportadora/app/src/main/java/transportadora/Administrador/Respuestas_seguridad/Editar_respuestas_seguridad.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Respuestas_seguridad/Editar_respuestas_seguridad.kt
@@ -1,10 +1,14 @@
 package transportadora.Administrador.Respuestas_seguridad
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Tel_administradores.Administrar_tel_administradores
 import transportadora.Login.R
 
 class Editar_respuestas_seguridad : AppCompatActivity() {
@@ -12,10 +16,17 @@ class Editar_respuestas_seguridad : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_editar_respuestas_seguridad)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnVolver = findViewById<TextView>(R.id.txt_volver_login)
+        btnVolver.setOnClickListener {
+            finish()
         }
+
+        val botoncrear = findViewById<Button>(R.id.buttonGuardar)
+        botoncrear.setOnClickListener {
+            val intent = Intent(this, Administrar_respuestas_seguridad::class.java)
+            startActivity(intent)
+        }
+
     }
 }
