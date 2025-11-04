a3636591fdf360cba1b9782eedd73456f869daaa
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-04 12:27:57 -0500

    con lineas vehiculo

--- Transportadora/app/src/main/java/transportadora/Administrador/Lineas_vehiculo/Crear_lineas_vehiculo.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Lineas_vehiculo/Crear_lineas_vehiculo.kt
@@ -1,10 +1,14 @@
 package transportadora.Administrador.Lineas_vehiculo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Estados_vehiculo.Administrar_estados_vehiculo
 import transportadora.Login.R
 
 class Crear_lineas_vehiculo : AppCompatActivity() {
@@ -12,10 +16,17 @@ class Crear_lineas_vehiculo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_crear_lineas_vehiculo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnVolver = findViewById<TextView>(R.id.txt_volver_login)
+        btnVolver.setOnClickListener {
+            finish()
         }
+
+        val botonguardar = findViewById<Button>(R.id.buttonGuardar)
+        botonguardar.setOnClickListener {
+            val intent = Intent(this, Administrar_lineas_vehiculo::class.java)
+            startActivity(intent)
+        }
+
     }
 }
