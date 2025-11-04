473f5b6fb717ba1c93a8cbe1c262d8f009b22430
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-04 14:20:36 -0500

    cat servicio

--- Transportadora/app/src/main/java/transportadora/Administrador/Categorias_servicio/Crear_categoria_servicio.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Categorias_servicio/Crear_categoria_servicio.kt
@@ -1,27 +1,34 @@
 package transportadora.Administrador.Categorias_servicio
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Administradores.Administrar_administradores
 import transportadora.Login.R
 
 class Crear_categoria_servicio : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_crear_categoria_servicio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val btnVolver = findViewById<TextView>(R.id.txt_volver_login)
         btnVolver.setOnClickListener {
             finish()
         }
+
+        val botonguardar = findViewById<Button>(R.id.buttonCrear)
+        botonguardar.setOnClickListener {
+            val intent = Intent(this, Administrar_categoria_servicio::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- Transportadora/app/src/main/java/transportadora/Administrador/Categorias_servicio/Editar_categoria_servicio.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Categorias_servicio/Editar_categoria_servicio.kt
@@ -1,11 +1,14 @@
 package transportadora.Administrador.Categorias_servicio
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Administradores.Administrar_administradores
 import transportadora.Login.R
 
 class Editar_categoria_servicio : AppCompatActivity() {
@@ -13,15 +16,17 @@ class Editar_categoria_servicio : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_editar_categoria_servicio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val btnVolver = findViewById<TextView>(R.id.txt_volver_login)
         btnVolver.setOnClickListener {
             finish()
         }
+
+        val botonguardar = findViewById<Button>(R.id.buttonGuardar)
+        botonguardar.setOnClickListener {
+            val intent = Intent(this, Administrar_categoria_servicio::class.java)
+            startActivity(intent)
+        }
+
     }
 }
