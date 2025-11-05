703287a4641fb3dbe5f762df861945769f49f843
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-04 19:16:16 -0500

    vehiculo

--- Transportadora/app/src/main/java/transportadora/Administrador/Vehiculos/Editar_vehiculos.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Vehiculos/Editar_vehiculos.kt
@@ -1,10 +1,14 @@
 package transportadora.Administrador.Vehiculos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Conductores.Administrar_conductores
 import transportadora.Login.R
 
 class Editar_vehiculos : AppCompatActivity() {
@@ -12,10 +16,17 @@ class Editar_vehiculos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_editar_vehiculos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnVolver = findViewById<TextView>(R.id.txt_volver_reg2)
+        btnVolver.setOnClickListener {
+            finish()
         }
+
+        val botoncrear = findViewById<Button>(R.id.buttonGuardar)
+        botoncrear.setOnClickListener {
+            val intent = Intent(this, Administrar_conductores::class.java)
+            startActivity(intent)
+        }
+
     }
 }
