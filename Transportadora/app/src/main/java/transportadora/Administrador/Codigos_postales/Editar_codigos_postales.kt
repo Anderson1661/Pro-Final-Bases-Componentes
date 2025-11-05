9c648b24d51e06eb83d8a2315568fcfaebc4a9c8
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-04 19:37:55 -0500

    codigo postal

--- Transportadora/app/src/main/java/transportadora/Administrador/Codigos_postales/Editar_codigos_postales.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Codigos_postales/Editar_codigos_postales.kt
@@ -1,27 +1,34 @@
 package transportadora.Administrador.Codigos_postales
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Lineas_vehiculo.Administrar_lineas_vehiculo
 import transportadora.Login.R
 
 class Editar_codigos_postales : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_editar_codigos_postales)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val btnVolver = findViewById<TextView>(R.id.txt_volver_login)
         btnVolver.setOnClickListener {
             finish()
         }
+
+        val botoncrear = findViewById<Button>(R.id.buttonGuardar)
+        botoncrear.setOnClickListener {
+            val intent = Intent(this, Administrar_lineas_vehiculo::class.java)
+            startActivity(intent)
+        }
+
     }
 }
