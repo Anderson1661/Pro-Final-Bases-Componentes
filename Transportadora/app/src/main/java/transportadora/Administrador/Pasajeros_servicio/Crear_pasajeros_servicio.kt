15a37f81a2ef651d3fd435096fdb9f6bff067479
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-04 19:25:58 -0500

    pasajeros ruta

--- Transportadora/app/src/main/java/transportadora/Administrador/Pasajeros_servicio/Crear_pasajeros_servicio.kt
+++ Transportadora/app/src/main/java/transportadora/Administrador/Pasajeros_servicio/Crear_pasajeros_servicio.kt
@@ -1,21 +1,34 @@
 package transportadora.Administrador.Pasajeros_servicio
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import transportadora.Administrador.Lineas_vehiculo.Administrar_lineas_vehiculo
 import transportadora.Login.R
 
 class Crear_pasajeros_servicio : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_crear_pasajeros_servicio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnVolver = findViewById<TextView>(R.id.txt_volver_login)
+        btnVolver.setOnClickListener {
+            finish()
         }
+
+        val botonguardar = findViewById<Button>(R.id.buttonGuardar)
+        botonguardar.setOnClickListener {
+            val intent = Intent(this, Administrar_lineas_vehiculo::class.java)
+            startActivity(intent)
+        }
+
     }
 }
