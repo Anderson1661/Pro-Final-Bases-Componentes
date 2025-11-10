7b4938d4d744f2287df4a6ab27d10fbb5ecab7d8
William Diaz <80552314+Williamddg@users.noreply.github.com>
2025-11-10 14:48:35 -0500

    estado conductor base y php

--- conexiones/estado_conductor/read.php
+++ conexiones/estado_conductor/read.php
@@ -5,7 +5,7 @@ $link = Conectar();
 $res = array();
 $res['datos'] = array();
 
-$sql = "SELECT * FROM conductor ORDER BY id_conductor";
+$sql = "SELECT * FROM estado_conductor ORDER BY id_estado_conductor";
 
 $res1 = mysqli_query($link, $sql);
 
@@ -13,18 +13,8 @@ if ($res1) {
     if (mysqli_num_rows($res1) > 0) {
         while ($row = mysqli_fetch_array($res1)) {
             $item = array();
-            $item['id_conductor'] = $row['id_conductor'];
             $item['id_estado_conductor'] = $row['id_estado_conductor'];
-            $item['placa_vehiculo'] = $row['placa_vehiculo'];
-            $item['identificacion'] = $row['identificacion'];
-            $item['id_tipo_identificacion'] = $row['id_tipo_identificacion'];
-            $item['nombre'] = $row['nombre'];
-            $item['direccion'] = $row['direccion'];
-            $item['correo'] = $row['correo'];
-            $item['id_genero'] = $row['id_genero'];
-            $item['codigo_postal'] = $row['codigo_postal'];
-            $item['id_pais_nacionalidad'] = $row['id_pais_nacionalidad'];
-            $item['url_foto'] = $row['url_foto'];
+            $item['descripcion'] = $row['descripcion'];
             array_push($res['datos'], $item);
         }
         $res["success"] = "1";
