ae34a81e0a25298e451f33f7366e366fff911f3e
Anderson1661 <144566153+Anderson1661@users.noreply.github.com>
2025-11-11 12:36:20 -0500

    conductor bueno

--- Transportadora/app/src/main/java/transportadora/Conductor/Principal_conductor.kt
+++ Transportadora/app/src/main/java/transportadora/Conductor/Principal_conductor.kt
@@ -5,17 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import transportadora.Conductor.ServiciosAdapter
 import transportadora.Login.R
 
 class Principal_conductor : AppCompatActivity() {
+    private lateinit var serviciosAdapter: ServiciosAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_principal_conductor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // 1. Configurar RecyclerView
+        val recyclerHistorial = findViewById<RecyclerView>(R.id.recyclerServicios_hoy)
+        recyclerHistorial.layoutManager = LinearLayoutManager(this)
+        serviciosAdapter = ServiciosAdapter(emptyList())
+        recyclerHistorial.adapter = serviciosAdapter
     }
 }
